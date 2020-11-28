1a2119dd592021b199651df794933076707d9b29
StasTODD <todd.stas@gmail.com>
2020-11-28 21:23:36 +0200

    Add: owners detection from DB

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from datetime import datetime
 import asyncio
 import aiohttp
 import yaml
-from typing import List
-
+from typing import List, Union
+import sqlite3
 import picamera
 from gpiozero import MotionSensor, Buzzer
 
@@ -18,6 +18,44 @@ This little program use 'gpiozero' library for work with GPIO pins. It's lib is 
 provide detail configurable properties. In the future, 'gpiozero' lib will be repplace to 'import RPi.GPIO as GPIO'. 
 RPi.GPIO libraty can work with signal interruptions.
 """
+
+
+# TODO: need import this def from '../smart_house/db/create_db.py'
+def create_connection(db_file: str) -> Union[type(sqlite3.connect), None]:
+    """
+    Create a database connection to the SQLite database specified by db_file
+
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+    return
+
+
+def check_owner_status() -> int:
+    # TODO: fix db path
+    database_name = "../smart_house/db/smart_house_db.db"
+    query = "SELECT at_home from OwnerStatus;"
+    conn = create_connection(database_name)
+    data = 0
+    if conn is not None:
+        try:
+            c = conn.cursor()
+            c.execute(query)
+            data = c.fetchall()
+            # db return data only in format: [(1,)]
+            data = data[0][0]
+        except sqlite3.Error as e:
+            print(e)
+    else:
+        print("Error! cannot create the database connection.")
+    conn.close()
+    return data
 
 
 def get_data_from_yaml(filename: str) -> dict:
@@ -85,9 +123,6 @@ def main():
 
     loop = asyncio.get_event_loop()
 
-    # Need extended functional for set this option:
-    access_flag = True
-
     # Url for send requests with photo:
     url = f"https://api.telegram.org/bot{tbot_api_token}/sendPhoto"
 
@@ -99,7 +134,7 @@ def main():
             sleep(1)
             # BUZZER_PIN.off()
 
-            if access_flag:
+            if not check_owner_status():
                 now = datetime.now()
                 image = create_photo(date=now)
                 with open(image, "rb") as b_image:
@@ -117,6 +152,8 @@ def main():
             # BUZZER_PIN.on()
             # sleep(0.15)
             # BUZZER_PIN.off()
+        else:
+            loop.close()
 
     except KeyboardInterrupt:
         print("Exit pressed Ctrl+C")
