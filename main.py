c44e6dd5b879ee444eede99c63decadf5f748987
Stas Goncharenko <todd.stas@gmail.com>
2023-02-21 00:15:31 +0200

    Chg: functional structure
    - data.yaml was refactored. Many configuration parameters were added
    - Buzzer code example was removed from main.py
    - def create_photo() was optimized
    - Extra IR highlight was added. It manages through data.yaml
    - Many comments and descriptions were added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/home/stastodd/projects/rpi_pirsensor_camera_telegram/venv/bin/python3.8
+#!/home/stastodd/projects/rpi_pirsensor_camera_telegram/venv/bin/python
 
 import sys
 import traceback
@@ -7,20 +7,11 @@ from datetime import datetime
 import asyncio
 import aiohttp
 import yaml
-from typing import List, Union
+from typing import Union
 import sqlite3
 import os
 import picamera
-from gpiozero import MotionSensor, Buzzer
-
-
-"""
-This little program use 'gpiozero' library for work with GPIO pins. It's lib is very easy for work, but this lib isn't 
-provide detail configurable properties. In the future, 'gpiozero' lib will be repplace to 'import RPi.GPIO as GPIO'. 
-RPi.GPIO libraty can work with signal interruptions.
-
-TODO: Exclude operation with file create if flag save_image=False
-"""
+from gpiozero import MotionSensor, Buzzer, OutputDevice
 
 
 # TODO: need import this def from '../smart_house/db/create_db.py'
@@ -35,16 +26,24 @@ def create_connection(db_file: str) -> Union[type(sqlite3.connect), None]:
     try:
         conn = sqlite3.connect(db_file)
         return conn
-    except Error as e:
-        print(e)
+    except sqlite3.Error as er:
+        print(er)
     return
 
 
-def check_owner_status() -> int:
-    # TODO: fix db path
-    database_name = "../smart_house/db/smart_house_db.db"
+def check_owner_status(smart_home_db_path="") -> int:
+    """
+    Connect to the smart_house_db.db and get data from table.
+
+    :param smart_home_db_path: "/home/stastodd/projects/smart_house/db/smart_house_db.db"
+    :return 1 | 0
+    """
+    if smart_home_db_path:
+        conn = create_connection(smart_home_db_path)
+    else:
+        return 0
+
     query = "SELECT at_home from OwnerStatus;"
-    conn = create_connection(database_name)
     data = 0
     if conn is not None:
         try:
@@ -56,7 +55,7 @@ def check_owner_status() -> int:
         except sqlite3.Error as e:
             print(e)
     else:
-        print("Error! cannot create the database connection.")
+        print("ERROR: Can't create the database connection")
     conn.close()
     return data
 
@@ -72,7 +71,7 @@ def get_data_from_yaml(filename: str) -> dict:
         return yaml.safe_load(f)
 
 
-async def send_image_to_tbot(url: str, one_data: List[dict]) -> str:
+async def send_image_to_tbot(url: str, one_data: dict) -> str:
     """
     Courutine with aiohttp lib for send request with image from RaspberryPI camera
 
@@ -84,93 +83,153 @@ async def send_image_to_tbot(url: str, one_data: List[dict]) -> str:
             return await response.text()
 
 
-def create_photo(date=None) -> str:
+def create_photo(filename=None, path="") -> str:
     """
-    :param date: date_time parameter. It's part of filename.jpg
-    :return: /home/username/filename.jpg
+    :param filename: name of the photo
+    :param path: "/home/stastodd/projects/rpi_pirsensor_camera_telegram/images"
+    :return: "/home/stastodd/projects/rpi_pirsensor_camera_telegram/images/20-02-2023_02:33:03.jpg"
     """
-    if not date:
-        now = datetime.now()
-        now_string = now.strftime("%d-%m-%Y_%H:%M:%S")
-    else:
-        now_string = date.strftime("%d-%m-%Y_%H:%M:%S")
+    current_datetime = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
-    imagename = f"images/{now_string}.jpg"
+    if filename:
+        if isinstance(filename, datetime):
+            filename = filename.strftime("%Y-%m-%d_%H:%M:%S")
+        else:
+            filename = current_datetime
+
+    if not path:
+        imagename = f"images/{filename}.jpg"
+    else:
+        imagename = f"{path}/{filename}.jpg"
 
     camera = picamera.PiCamera()
     # Picture timestamp:
     camera.annotate_text_size = 40
-    camera.annotate_text = " " * 42 + " ".join(now_string.split("_"))
+    camera.annotate_text = " " * 42 + " ".join(current_datetime.split("_"))
     # TODO: Add more camera settings
     try:
         camera.capture(imagename)
     except Exception as all_exceptions:
-        print("--- Start Exception Data:")
         traceback.print_exc(limit=2, file=sys.stdout)  # Exception detail via traceback
-        print("--- End Exception Data:")
     finally:
         camera.close()
         return imagename
 
 
-def main(save_image=False):
-    # Fixme: Remove strings with BUZZER_PIN actions after correcting PIR-sensor position
-    PIR_PIN = MotionSensor(4)
-    # BUZZER_PIN = Buzzer(17)
+def main(config_data):
+    """
+    :param config_data: {
+        'admins_ids': [{'stastodd': 1}, {'username': 2}],
+        'api_telegram_token': '3',
+        'rpi_pirsensor_camera_telegram_params': {
+            'enable_status': True,
+            'save_photo_image_to_file': False,
+            'photo_storage_path': '/home/stastodd/projects/rpi_pirsensor_camera_telegram/images'
+            'motion_sensor_1_pin': 4,
+            'pause_between_photo_sec': 10
+            'use_extra_highlight': True,
+            'extra_highlight_1_pin': 26,
+            'use_buzzer_notification': False,
+            'buzzer_notification_pin': 17,
+            'use_smart_home_db': True,
+            'smart_home_db': '/home/stastodd/projects/smart_house/db/smart_house_db.db'}}
+    """
+    # Get all config params:
+    api_telegram_token = config_data.get("api_telegram_token")
+    admins_ids = config_data.get("admins_ids", list())
 
-    tbot_data_all = get_data_from_yaml("data.yaml")
+    rpi_pirsensor_camera_telegram_data = config_data.get("rpi_pirsensor_camera_telegram_params", dict())
 
-    tbot_api_token = tbot_data_all.get("api_token")
-    admins_ids = tbot_data_all.get("admins_ids")
-    if isinstance(admins_ids, list):
-        admins_ids = [list(adm_id.values())[0] for adm_id in admins_ids]
+    # Config params:
+    enable_status = rpi_pirsensor_camera_telegram_data.get("enable_status", True)
+    save_photo_image_to_file = rpi_pirsensor_camera_telegram_data.get("save_photo_image_to_file", False)
+    photo_storage_path = rpi_pirsensor_camera_telegram_data.get("photo_storage_path", str())
+    motion_sensor_1_pin = rpi_pirsensor_camera_telegram_data.get("motion_sensor_1_pin")
+    pause_between_photo_sec = rpi_pirsensor_camera_telegram_data.get("pause_between_photo_sec", 10)
+    use_extra_highlight = rpi_pirsensor_camera_telegram_data.get("use_extra_highlight", False)
+    extra_highlight_1_pin = rpi_pirsensor_camera_telegram_data.get("extra_highlight_1_pin")
+    use_buzzer_notification = rpi_pirsensor_camera_telegram_data.get("use_buzzer_notification", False)
+    buzzer_notification_pin = rpi_pirsensor_camera_telegram_data.get("buzzer_notification_pin")
+    use_smart_home_db = rpi_pirsensor_camera_telegram_data.get("use_smart_home_db", False)
+    smart_home_db_path = rpi_pirsensor_camera_telegram_data.get("smart_home_db_path", str())
+
+    # Url for send requests with photomessage:
+    url_photomessage = f"https://api.telegram.org/bot{api_telegram_token}/sendPhoto"
+
+    if not enable_status:
+        return
 
     loop = asyncio.get_event_loop()
 
-    # Url for send requests with photo:
-    url = f"https://api.telegram.org/bot{tbot_api_token}/sendPhoto"
+    if isinstance(admins_ids, list):
+        admins_ids = [list(adm_id.values())[0] for adm_id in admins_ids]
+
+    # Extra highlight for noIR camera:
+    highlight_1 = None
+    if use_extra_highlight and extra_highlight_1_pin:
+        highlight_1 = OutputDevice(extra_highlight_1_pin, active_high=True, initial_value=False)
+    # Motion sensor:
+    motion_sensor_1 = None
+    if motion_sensor_1_pin:
+        motion_sensor_1 = MotionSensor(motion_sensor_1_pin)
+    # Buzzer speaker for sounds notifications:
+    buzzer_device = None
+    if use_buzzer_notification and buzzer_notification_pin:
+        buzzer_device = Buzzer(buzzer_notification_pin)
 
     try:
         while True:
-            PIR_PIN.wait_for_motion()
-            print("You moved")
-            # BUZZER_PIN.on()
-            sleep(1)
-            # BUZZER_PIN.off()
+            # Waiting for motion from PIR sensor. When motion is detected, program goes further:
+            if motion_sensor_1_pin:
+                motion_sensor_1.wait_for_motion()
 
-            if not check_owner_status():
-                now = datetime.now()
-                image = create_photo(date=now)
+            sleep(1)
+
+            make_photo_status = True
+            # Check permission to use smart_home_db.db:
+            if use_smart_home_db:
+                if check_owner_status(smart_home_db_path=smart_home_db_path):
+                    make_photo_status = False
+
+            if make_photo_status:
+                # Enable extra highlight:
+                if use_extra_highlight:
+                    highlight_1.on()
+                    sleep(0.1)
+
+                current_datetime = datetime.now()
+
+                # Take a photo and transform it into binary file:
+                image = create_photo(filename=current_datetime, path=photo_storage_path)
                 with open(image, "rb") as b_image:
                     b_image = b_image.read()
-                if isinstance(admins_ids, list):
-                    all_data = [{"chat_id": str(adm), "photo": b_image} for adm in admins_ids]
-                    coroutines = [send_image_to_tbot(url, one_data) for one_data in all_data]
-                    loop.run_until_complete(asyncio.gather(*coroutines))
-                if not save_image:
+
+                # Disable extra highlight:
+                if use_extra_highlight:
+                    highlight_1.off()
+
+                # async photo sending to telegram bot:
+                data_to_send_list = [{"chat_id": str(adm), "photo": b_image} for adm in admins_ids]
+                coroutines = [send_image_to_tbot(url_photomessage, one_data) for one_data in data_to_send_list]
+                loop.run_until_complete(asyncio.gather(*coroutines))
+
+                # Remove photo image from storage:
+                if not save_photo_image_to_file:
                     try:
                         os.remove(image)
-                    except:
-                        print(f"File {image} can't be deleted")
+                    except OSError:
+                        print(f"FAIL: '{image}' file can't be deleted")
 
-            PIR_PIN.wait_for_no_motion(timeout=10)
-            # BUZZER_PIN.on()
-            # sleep(0.15)
-            # BUZZER_PIN.off()
-            # sleep(0.2)
-            # BUZZER_PIN.on()
-            # sleep(0.15)
-            # BUZZER_PIN.off()
-        else:
-            loop.close()
-
+                # Pause between photo:
+                motion_sensor_1.wait_for_no_motion(timeout=int(pause_between_photo_sec))
     except KeyboardInterrupt:
         print("Exit pressed Ctrl+C")
     except Exception as all_exceptions:
-        print("--- Start Exception Data:")
+        print("--- Start Global Exception")
         traceback.print_exc(limit=2, file=sys.stdout)  # Exception detail via traceback
-        print("--- End Exception Data:")
+        print("--- End Global Exception")
 
 
 if __name__ == "__main__":
-    main(save_image=False)
+    config_data = get_data_from_yaml("data.yaml")
+    main(config_data)
